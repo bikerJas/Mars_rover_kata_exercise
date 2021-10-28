40653734673806eb47f95cb400684b50db31832e
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-10-28 11:49:47 +0100

    tested with two rovers

--- src/marsrover.test.js
+++ src/marsrover.test.js
@@ -2,7 +2,7 @@ const newLocation = require("./marsrover");
 
 describe("newLocation", () => {
   test("returns array of new locations", () => {
-    const rovers = [[5,5],[[3,3,"E"],["MMRMMRMRRM"]]]
-    expect(newLocation(rovers)).toEqual([[5, 1, "E"]]);
+    const rovers = [[5,5],[[1,2,"N"],["LMLMLMLMM"]],[[3,3,"E"],["MMRMMRMRRM"]]]
+    expect(newLocation(rovers)).toEqual([[1, 3, "N"],[5, 1, "E"]]);
   });
 });
