be0d35b7890226a5b0a163893f6792c756056501
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-04 13:41:13 +0000

    Missing rover starting position data test passing

--- src/roverValidation.js
+++ src/roverValidation.js
@@ -1,8 +1,10 @@
 function roverValidate(robotPositions) {
-    const notValid = /[^LRM]/g;
-    const noMovement = /[M]/g;
-    const compassPoints = ["N", "S", "W", "E"]
+  const notValid = /[^LRM]/g;
+  const noMovement = /[M]/g;
+  const compassPoints = ["N", "S", "W", "E"];
 
+  if (robotPositions[0].length !== 3)
+    throw new Error("Rover coordinates invalid");
 }
 
-    module.exports = roverValidate;  
+module.exports = roverValidate;
