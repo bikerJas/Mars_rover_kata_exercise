72038d0ec128a264377c019deb820829c51ca474
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-04 12:35:09 +0000

    Test for plateau data is now passing

--- src/mars_plateau.js
+++ src/mars_plateau.js
@@ -1,5 +1,5 @@
 function plateauSize(rovers) {
-    
+  if (rovers[0].length !== 2) throw new Error("Plateau size not valid");
 }
 
 module.exports = plateauSize;
--- src/marsrover.js
+++ src/marsrover.js
@@ -4,6 +4,9 @@ function newLocation(rovers) {
   if (!rovers) throw new Error("No co-ordinates supplied for rovers");
   let newRoverPosition = [];
 
+  //Validate the data received for the plateau is all present and in the correct format
+  plateauSize(rovers);
+
   return newRoverPosition;
 }
 
