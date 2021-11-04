64fbb7e02105500653aecf4c61c3a18c98c258ae
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-04 12:46:22 +0000

    Passing test for checking plateau data is supplied in correct format

--- src/mars_plateau.js
+++ src/mars_plateau.js
@@ -1,5 +1,7 @@
 function plateauSize(rovers) {
   if (rovers[0].length !== 2) throw new Error("Plateau size not valid");
+  if (isNaN(rovers[0][0]) || isNaN(rovers[0][1]))
+    throw new Error("Invalid data for Plateau");
 }
 
 module.exports = plateauSize;
